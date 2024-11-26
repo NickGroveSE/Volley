d8751508d535fbff1eb87b81b8b52d15f2e44b0d
Nick Grove <nickgrove.se@gmail.com>
2024-11-26 12:40:19 -0500

    Another PyWin Change

--- app.py
+++ app.py
@@ -1,14 +1,8 @@
 from flask import Flask, jsonify
 from flask_cors import CORS, cross_origin
-import requests
-import os
-from dotenv import load_dotenv, dotenv_values
-from html.parser import HTMLParser
-import re
 
 app = Flask(__name__)
 CORS(app, support_credentials=True)
-load_dotenv()
 
 
 @app.route("/")
